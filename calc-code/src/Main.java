1b3211ace4df1bf3e4428051adddb36bb1f6b4d8
daniele duarte <duartedaniele75@gmail.com>
2025-04-03 14:54:16 -0400

    adding changes to the main.java code to the repository

--- calc-code/src/Main.java
+++ calc-code/src/Main.java
@@ -1,34 +1,46 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
 
         Scanner user = new Scanner(System.in);
+        boolean continueLoop = true;
 
         System.out.println("DO A CALCULATION!");
-        System.out.println("Enter the first number: ");
-        double numberOne = user.nextDouble();
-        System.out.println("Enter the second number: ");
-        double numberTwo= user.nextDouble();
-        System.out.println("Choose an operation: (+, -, *, /");
-        char operator = user.next().charAt(0);
 
-        if (operator == '+') {
-            double result = numberOne + numberTwo;
-            System.out.printf("%.1f + %.1f = %.1f", numberOne, numberTwo, result);
-        }
-        else if (operator == '-') {
-            double result = numberOne - numberTwo;
-            System.out.printf("%.1f - %.1f = %.1f", numberOne, numberTwo, result);
-        }
-        else if (operator == '*') {
-            double result = numberOne * numberTwo;
-            System.out.printf("%.1f * %.1f = %.1f", numberOne, numberTwo, result);
-        }
-        else {
-            double result = numberOne / numberTwo;
-            System.out.printf("%.1f / %.1f = %.1f", numberOne, numberTwo, result);
-        }
+    while (continueLoop) {
+            System.out.println("Enter the first number: ");
+            double numberOne = user.nextDouble();
+            System.out.println("Enter the second number: ");
+            double numberTwo = user.nextDouble();
+            System.out.println("Choose an operation: (+, -, *, /)");
+            char operator = user.next().charAt(0);
+            double result;
 
+            switch (operator) {
+                case '+' : result = numberOne + numberTwo; break;
+                case '-' : result = numberOne - numberTwo; break;
+                case '*' : result = numberOne * numberTwo; break;
+                case '/' : result = numberOne / numberTwo;
+                    if (numberTwo != 0 ) {
+                        result = numberOne / numberTwo; }
+                    else {
+                        System.out.println("Error: Cannot divide by zero!");
+                        continue;
+                    } break;
+                    default:
+                    System.out.println("Invalid Operation");
+                    continue;
+            }
+            System.out.println("Result: " + result);
+            System.out.println("Do you wanna continue: (S/N)");
+            String respLoop = user.next().toUpperCase();
+            if (respLoop.equals("N")) {
+                continueLoop = false;
+            }
+        }
+        System.out.println("Calculator closed.");
+        user.close();
     }
 }
